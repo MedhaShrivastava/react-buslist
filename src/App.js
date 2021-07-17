19966a9ed828a3abbf31aad8003c39b0fe73a47c
MedhaShrivastava <medhakumari299@gmail.com>
2021-07-17 10:47:13 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ function CardDisplay() {
   return (
     <CardColumns>
       <Card>
-        <Card.Img variant="top" src="http://askavy.com/demo/img/img-card.jpg" />
+        <Card.Img variant="top" src=" https://user-images.githubusercontent.com/87075276/126026510-342d1062-9372-49de-8020-1116a53202ce.jpg" />
         <Card.Body>
-          <Card.Title>Card title </Card.Title>
+          <Card.Title>Rupa Kirana Store </Card.Title>
           <Card.Text>
-            Card Text This card has supporting text below as a natural lead-in
-            to additional content.{" "}
+            Address: Jalalpur city 
+            <br/>
+            Name of Owner: Ram Prashad
+            <br/>
+            Contact No.: 76895432
+            <br/>
+            Email-id: rupakirana122@gmail.com
+            <br/>
+            Looking For: Salesman ; Delivery Boy 
+            {" "}
           </Card.Text>
         </Card.Body>
         <Card.Footer>
